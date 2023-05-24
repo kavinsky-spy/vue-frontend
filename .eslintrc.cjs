9e542e1ab80bc2d5ad1748eecca11f0e1e25994a
julioretkwa <julio@zoocha.com>
2023-05-24 01:14:06 -0300

    initial config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,41 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+  env: {
+    browser: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+    'plugin:tailwindcss/recommended',
+    'prettier',
+    'plugin:vue/vue3-recommended',
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: false,
+    },
+  },
+  plugins: ['@typescript-eslint', 'unused-imports', 'prettier'],
+  rules: {
+    'vue/max-len': [
+      'error',
+      {
+        code: 80,
+        ignoreHTMLAttributeValues: true,
+      },
+    ],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+        },
+      },
+    ],
+  },
 }
